85e0d961076b686bdb193facc7f2b86a41d4d087
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2021-11-24 22:36:23 +0100

    Añadidas variables y su documentación

--- Practica5_ejercicio2/main.js
+++ Practica5_ejercicio2/main.js
@@ -4,7 +4,25 @@
  * Si acierta, o si se acabaron sus intentos, se le pregunta si quiere seguir jugando
  */
 function _main_() {
-
+    /**
+     * Nombre del número del usuario para acertar
+     * @type {integer} 
+     */
+    var numero_usuario = 0;
+    /**
+     * Nombre de la respuesta recibida a la pregunta de si quiere continuar
+     * @type {integer} 
+     */
+    var mensaje_continuar = "";
+    /**
+     * Nombre del mensaje para avisar al usuario de si el número que busca es mayor o menor 
+     * @type {string} 
+     */
+    var mensaje_menor_mayor = "";
+    /** Nombre que recibe la posibilidad o no de que el usuario contiue jugando
+     * @type {boolean} 
+     */
+    var isRunning = false;
 
 }
 
