e1080a68b008b4eca202c15377afd16f4d2e07b2
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2021-11-24 22:30:20 +0100

    Comentario función princpal main.js

---
+++ Practica5_ejercicio2/main.js
@@ -0,0 +1,5 @@
+/**
+ * Menú principal del programa; genera un número aleatorio y da al usuario la posibilidad de acertarlo en 5 intentos
+ * Indica al usuario si si el número que busca es mayor o menor al que acaba de introducir
+ * Si acierta, o si se acabaron sus intentos, se le pregunta si quiere seguir jugando
+ */
