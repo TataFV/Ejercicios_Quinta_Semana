75e022ec0c879a04e1148780869f50caadb6c675
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2021-11-24 22:58:20 +0100

    Añadida fn comprobar_menor_mayor ( )

--- Practica5_ejercicio2/azar.js
+++ Practica5_ejercicio2/azar.js
@@ -10,3 +10,21 @@ function crear_aleatorio() {
     var numero_aleatorio = Math.floor(Math.random() * 100) + 1;
     return numero_aleatorio
 }
+/**
+ * Comprueba si el número aleatorio es menor o mayor que el que introdujo el usuario
+ * @param {integer} aleatorio - Número aleatorio creado
+ * @param {integer} usuario - Número introducido por el usuario
+ * @param {string} mensaje - Comunica al usuario si el número es mayor, menor o si ha acertado 
+ */
+
+function comprobar_menor_mayor(aleatorio, usuario) {
+    var mensaje;
+    if (usuario > aleatorio) {
+        mensaje = "El número que buscas es menor"
+    } else if (usuario < aleatorio) {
+        mensaje = "El número que buscas es mayor"
+    } else {
+        mensaje = "Has acertado"
+    }
+    return mensaje
+}
