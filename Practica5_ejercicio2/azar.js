28efbb9e5853368f9b11c344c9fd5d04c5d0ea8b
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2021-11-24 22:39:42 +0100

    Añadido fichero azar.js y la fn crear_aleatorio( )

---
+++ Practica5_ejercicio2/azar.js
@@ -0,0 +1,12 @@
+/** 
+ * Devuelve un número aleatorio
+ * @returns {integer} número aleatorio 
+*/
+function crear_aleatorio() {
+    /**
+     * Nombre del número aleatorio 
+     * @type {integer}
+    */
+    var numero_aleatorio = Math.floor(Math.random() * 100) + 1;
+    return numero_aleatorio
+}
